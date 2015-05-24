d397c43f817021e17c371f3192b52a57937310e2
Michael Sarchet <mikesarchet@gmail.com>
2015-05-24 13:49:23 -0700

    Add glassy example

---
+++ example/glassy/glassfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paths: {  
+    javascript : ['./src/**/*.js'], 
+    css : ['./src/**/*.css']
+  },
+  relative: ['./src'],
+  templatePath : './src/template.jade',
+  output : './index.html'
+}
